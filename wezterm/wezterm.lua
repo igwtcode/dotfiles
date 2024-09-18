8a2430b2fd800f539bba9c4955726ae245ceb83a
igwtcode <igwtcode@gmail.com>
2024-09-19 00:00:09 +0200

    wezterm
    font
    theme
    audible_bell Disabled

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,8 @@ local act = wezterm.action
 local config = {}
 -- local font_name = "JetBrainsMonoNL Nerd Font"
 -- local font_name = "CaskaydiaCove Nerd Font"
-local font_name = "UbuntuMono Nerd Font"
+-- local font_name = "UbuntuMono Nerd Font"
+local font_name = "MesloLGM Nerd Font"
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
@@ -11,8 +12,9 @@ end
 
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "Catppuccin Macchiato"
-		-- return "Catppuccin Mocha"
+		-- return "Tokyo Night"
+		-- return "Catppuccin Macchiato"
+		return "Catppuccin Mocha"
 	else
 		return "Catppuccin Latte"
 	end
@@ -20,9 +22,9 @@ end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
--- config.macos_window_background_blur = 30
-config.native_macos_fullscreen_mode = true
-config.window_background_opacity = 0.85
+config.macos_window_background_blur = 18
+config.native_macos_fullscreen_mode = false
+config.window_background_opacity = 0.77
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.window_frame = { font_size = 16 }
@@ -45,12 +47,13 @@ config.font = wezterm.font_with_fallback({
 	font_name,
 	{ family = font_name, italic = true },
 })
-config.font_size = 20
-config.line_height = 1.2
+config.font_size = 17
+-- config.line_height = 1.1
 config.dpi = 144
 config.initial_cols = 90
 config.initial_rows = 30
 config.scrollback_lines = 6000
+config.audible_bell = "Disabled"
 
 config.keys = {
 	{
