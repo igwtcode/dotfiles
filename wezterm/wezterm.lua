ec2c885d3a86e5fa26cdf7b9c1adad124d766fb9
igwtcode <igwtcode@gmail.com>
2024-10-01 00:56:27 +0200

    wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -22,17 +22,17 @@ end
 
 config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
 
-config.macos_window_background_blur = 24
+config.macos_window_background_blur = 33
 config.native_macos_fullscreen_mode = false
-config.window_background_opacity = 0.72
+config.window_background_opacity = 0.77
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.window_frame = { font_size = 16 }
 config.window_padding = {
-	left = 0,
-	bottom = 0,
-	top = 0,
-	right = 0,
+	left = 9,
+	bottom = 6,
+	top = 6,
+	right = 9,
 }
 
 config.adjust_window_size_when_changing_font_size = false
@@ -40,8 +40,9 @@ config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = true
 config.hide_tab_bar_if_only_one_tab = true
 
-config.default_cursor_style = "SteadyBlock"
+-- config.default_cursor_style = "SteadyBlock"
 -- config.default_cursor_style = "BlinkingBar"
+config.default_cursor_style = "BlinkingUnderline"
 -- config.cursor_thickness = 3
 config.force_reverse_video_cursor = true
 
